8a6868552413325f4990be6dcbcb90fea04f4cfa
Ian Honorato <ian.0603@hotmail.com>
2021-05-26 13:38:49 -0300

    Adaptando posições para o padrão do xadrez

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -8,14 +8,20 @@ namespace ConsoleChess
     {
         static void Main(string[] args)
         {
-            Board board = new Board(8, 8);
+            /*Board board = new Board(8, 8);
 
             board.putPiece(new Rook(board, Color.Black), new Position(0, 0));
             board.putPiece(new Queen(board, Color.Black), new Position(3, 4));
             board.putPiece(new King(board, Color.Black), new Position(0, 0));
-
+            
             Screen.printBoard(board);
 
+            */
+            ChessPosition pos = new ChessPosition('c', 7);
+            Console.WriteLine(pos.toPosition());
+            Console.WriteLine(pos);
+
+            
             Console.ReadLine();
         }
     }
