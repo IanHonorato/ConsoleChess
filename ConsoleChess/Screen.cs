0c46dd6271c0170e7925d1334aa23b601a96da4c
Ian Honorato <ian.0603@hotmail.com>
2021-05-26 13:46:20 -0300

    Melhorando a impressão do tabuleiro

--- ConsoleChess/Screen.cs
+++ ConsoleChess/Screen.cs
@@ -12,6 +12,7 @@ namespace ConsoleChess
 
         public static void printBoard(Board board) {
             for (int i = 0; i < board.lines; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.columns; j++) {
                     if (board.piece(i, j) == null)
                     {
@@ -19,11 +20,25 @@ namespace ConsoleChess
                     }
                     else
                     {
-                        Console.Write(board.piece(i, j) + " ");
+                        printPiece(board.piece(i, j));
                     }
                 }
 
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void printPiece(Piece piece) {
+            if (piece.color == Color.White)
+            {
+                Console.Write(piece);
+            }
+            else if (piece.color == Color.Black) {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
