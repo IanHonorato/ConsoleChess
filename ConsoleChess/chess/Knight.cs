eff0ca5b4665ff59720e4abb80f1854ad89dd30b
Ian Honorato <ian.0603@hotmail.com>
2021-06-01 15:07:40 -0300

    Implementando as demais peças, organizando o tabuleiro de xadrez

--- ConsoleChess/chess/Knight.cs
+++ ConsoleChess/chess/Knight.cs
@@ -13,7 +13,49 @@ namespace chess
 
         public override bool[,] availableMovements()
         {
-            throw new NotImplementedException();
+            bool[,] mat = new bool[board.lines, board.columns];
+
+            Position pos = new Position(0, 0);
+
+            pos.setValues(position.line - 1, position.column - 2);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line - 2, position.column - 1);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line - 2, position.column + 1);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line - 1, position.column + 2);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line + 1, position.column + 2);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line + 2, position.column + 1);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line + 2, position.column - 1);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            pos.setValues(position.line + 1, position.column - 2);
+            if (board.validPosition(pos) && canMove(pos))
+                mat[pos.line, pos.column] = true;
+
+            return mat;
+        }
+
+        private bool canMove(Position pos)
+        {
+            Piece p = board.piece(pos);
+            return p == null || p.color != color;
         }
 
         public override string ToString()
---
+++ ConsoleChess/chess/Pawn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using board;
+
+namespace chess
+{
+    class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(board, color) { }
+
+        public override bool[,] availableMovements()
+        {
+            bool[,] mat = new bool[board.lines, board.columns];
+
+            Position pos = new Position(0,0);
+
+            if(color == Color.White)
+            {
+                pos.setValues(position.line - 1, position.column);
+                if (board.validPosition(pos) && freePawn(pos))
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line - 2, position.column);
+                if (board.validPosition(pos) && freePawn(pos) && nMoves == 0)
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line - 1, position.column - 1);
+                if (board.validPosition(pos) && hasEnemy(pos))
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line - 1, position.column + 1);
+                if (board.validPosition(pos) && hasEnemy(pos))
+                    mat[pos.line, pos.column] = true;
+            } 
+            else
+            {
+                pos.setValues(position.line + 1, position.column);
+                if (board.validPosition(pos) && freePawn(pos))
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line + 2, position.column);
+                if (board.validPosition(pos) && freePawn(pos) && nMoves == 0)
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line + 1, position.column - 1);
+                if (board.validPosition(pos) && hasEnemy(pos))
+                    mat[pos.line, pos.column] = true;
+
+                pos.setValues(position.line + 1, position.column + 1);
+                if (board.validPosition(pos) && hasEnemy(pos))
+                    mat[pos.line, pos.column] = true;
+            }
+
+            return mat;
+        }
+
+        private bool hasEnemy(Position pos)
+        {
+            Piece p = board.piece(pos);
+            return p != null && p.color != color;
+        }
+
+        public bool freePawn(Position pos)
+        {
+            return board.piece(pos) == null;
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+    }
+}
